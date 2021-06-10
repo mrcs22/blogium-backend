0dc1a48697715610cb51f1dae78f3acf493d70ca
mrcs22 <marcusmoraes2010@hotmail.com>
2021-06-09 21:32:35 -0300

    Persisting data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
 
@@ -7,10 +8,16 @@ app.use(express.json());
 
 app.use(cors());
 
-const posts = ["a", "b"];
-const comments = [];
+const data = fs.existsSync("./src/data.json")
+  ? JSON.parse(fs.readFileSync("./src/data.json"))
+  : { posts: [], comments: [] };
+
+const posts = data.posts;
+const comments = data.comments;
 
 app.get("/posts", (req, res) => {
+  console.log("get at /posts");
+  console.log(posts);
   res.send(posts);
 });
 
@@ -26,6 +33,7 @@ app.get("/posts/:id", (req, res) => {
 app.post("/posts", (req, res) => {
   const post = req.body;
   posts.push(post);
+  fs.writeFileSync("./src/data.json", JSON.stringify(data));
   res.send("ok");
 });
 
